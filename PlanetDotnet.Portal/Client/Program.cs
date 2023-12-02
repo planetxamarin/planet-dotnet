1370e7bd670ec6d53c89cb0d8b2bc210a9433101
Mabrouk.Mahdhi <Mabrouk.Mahdhi@ebizcon.de>
2023-12-02 02:43:19 +0100

    INFRA: Initialize Project

---
+++ PlanetDotnet.Portal/Client/Program.cs
@@ -0,0 +1,28 @@
+// ---------------------------------------------------------------
+// Copyright (c) 2023 Planet Dotnet. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace PlanetDotnet.Portal
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
+
+            builder.Services.AddScoped(sp => new HttpClient
+            {
+                BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+            });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
