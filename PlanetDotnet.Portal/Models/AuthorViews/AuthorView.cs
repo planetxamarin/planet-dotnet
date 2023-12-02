d4d7b626a046a3af5e51a0122888cc17e58e173a
Mabrouk.Mahdhi <Mabrouk.Mahdhi@ebizcon.de>
2023-12-02 20:54:22 +0100

    DATA: AuthorView

---
+++ PlanetDotnet.Portal/Models/AuthorViews/AuthorView.cs
@@ -0,0 +1,22 @@
+// ---------------------------------------------------------------
+// Copyright (c) 2023 Planet Dotnet. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System;
+using PlanetDotnet.Portal.Models.GeoPositions;
+
+namespace PlanetDotnet.Portal.Models.AuthorViews
+{
+    public class AuthorView
+    {
+        public string DisplayName { get; set; }
+        public string Location { get; set; }
+        public string ShortBioOrTagLine { get; set; }
+        public Uri WebSite { get; set; }
+        public string TwitterHandle { get; set; }
+        public string GravatarUrl { get; set; }
+        public GeoPosition Position { get; set; }
+    }
+}
