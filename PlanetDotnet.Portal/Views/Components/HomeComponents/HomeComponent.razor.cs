bd659a0bfbb6eca3809d18203087245626c46345
Mabrouk.Mahdhi <Mabrouk.Mahdhi@ebizcon.de>
2023-12-02 20:06:12 +0100

    VIEWS: Home

---
+++ PlanetDotnet.Portal/Views/Components/HomeComponents/HomeComponent.razor.cs
@@ -0,0 +1,13 @@
+// ---------------------------------------------------------------
+// Copyright (c) 2023 Planet Dotnet. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using Microsoft.AspNetCore.Components;
+
+namespace PlanetDotnet.Portal.Views.Components.HomeComponents
+{
+    public partial class HomeComponent : ComponentBase
+    { }
+}
