361ad8b46372563868dd5ce595c5599c60e8d4b5
Mabrouk.Mahdhi <Mabrouk.Mahdhi@ebizcon.de>
2023-12-02 20:09:33 +0100

    DATA: Author

---
+++ PlanetDotnet.Portal/Models/Authors/Author.cs
@@ -0,0 +1,28 @@
+// ---------------------------------------------------------------
+// Copyright (c) 2023 Planet Dotnet. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using PlanetDotnet.Portal.Models.GeoPositions;
+
+namespace PlanetDotnet.Portal.Models.Authors
+{
+    public class Author
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string StateOrRegion { get; set; }
+        public string EmailAddress { get; set; }
+        public string ShortBioOrTagLine { get; set; }
+        public Uri WebSite { get; set; }
+        public string TwitterHandle { get; set; }
+        public string GitHubHandle { get; set; }
+        public string GravatarHash { get; set; }
+        public IEnumerable<Uri> FeedUris { get; set; }
+        public GeoPosition Position { get; set; }
+        public string FeedLanguageCode { get; set; }
+    }
+}
