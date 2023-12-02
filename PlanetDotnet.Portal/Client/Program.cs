aee9e8433d97deb2bde7d2272c0a4ff1bf803179
Mabrouk.Mahdhi <Mabrouk.Mahdhi@ebizcon.de>
2023-12-02 02:47:24 +0100

    CODE RUB: Clean up

--- PlanetDotnet.Portal/Client/Program.cs
+++ PlanetDotnet.Portal/Client/Program.cs
@@ -17,10 +17,7 @@ namespace PlanetDotnet.Portal
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
-            builder.Services.AddScoped(sp => new HttpClient
-            {
-                BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
-            });
+            builder.Services.AddScoped(sp => new HttpClient());
 
             await builder.Build().RunAsync();
         }
